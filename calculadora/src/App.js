8039420a4a2a34a69e39803f06eaf23061b07829
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2023-12-20 10:43:41 -0500

    inicio proyecto calculadora

---
+++ calculadora/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import logo from './images/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='logo-container'>
+        <img
+        src={logo}
+        className='logo'
+        alt='Logo de la aplicación'
+        />
+      </div>
+      <div className='main-container'>
+          
+        </div>
+    </div>
+  );
+}
+
+export default App;
