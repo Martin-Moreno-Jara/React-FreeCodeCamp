08f72d3cce3d05d414cff3255efc158b27550769
Martin-Moreno-Jara <martin.moreno.j04@gmail.com>
2023-12-27 21:01:29 -0500

    manejar eventos de escritura

--- task-manager/src/components/Form.js
+++ task-manager/src/components/Form.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../stylesheets/Form.css'
 
 function Form(props){
+    const [input,setInput] = useState('');
+
+    const manageChange = (e)=>
+    {
+     setInput(e.target.value);   
+    }
+
+    const manageSignal = (e)=>
+    {
+        const newTask = 
+        {
+            id:'3548',
+            text:{input}
+        }
+
+    };
+
     return(
         <div className='task-form'>
             <input 
@@ -9,9 +26,13 @@ function Form(props){
             type='text'
             placeholder='Escribe una tarea'
             name='text'
+            onChange={manageChange}
             />
 
-            <button className='task-button'>
+            <button 
+            className='task-button'
+            onClick={manageSignal}
+            >
             Agregar Tarea
             </button>
             
--- task-manager/src/components/TaskList.js
+++ task-manager/src/components/TaskList.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import '../stylesheets/TaskList.css'
 import Form from './Form';
+import Task from './Task';
 
 function TaskList(){
+    const [tasks,setTasks] = useState([]);
+
+    const addTask = (task)=>{
+        console.log('Task succesfully added');
+        console.log(task)
+    };
+
+    const completeTask = (id)=>{
+
+    };
+    
+    const deleteTask =(id)=>{
+
+    };
+
     return(
         <>
             <Form/>
             <div className='taskList-container'>
-                Lista de tareas
+                {
+                  tasks.map(
+                    (task)=>
+                    {<Task
+                        text={tasks.text}  
+                        isCompleted={tasks.isCompleted}  />}
+                  )  
+                }
 
             </div>
         </>
